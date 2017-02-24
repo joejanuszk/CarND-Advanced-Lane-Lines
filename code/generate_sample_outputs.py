cd76fa3e9cdcc688e4b3c4b49bd63a3f24f792fc
Joe Januszkiewicz <joe.januszk@gmail.com>
2017-02-23 23:01:18 -0500

    added a figure showing a warped thresholded image with polynomial fit overlaid

--- code/generate_sample_outputs.py
+++ code/generate_sample_outputs.py
@@ -1,9 +1,11 @@
 import glob
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 from process_image import correct_distortion, thresholded_image, warp_perspective
+from find_lane_lines import find_lines_via_histogram_peaks
+from plotting_utils import get_plotting_values_for_fits
 
-'''
 img_bgr = cv2.imread('../test_images/test4.jpg')
 img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
 img_corrected = correct_distortion(img_rgb)
@@ -11,6 +13,27 @@ img_thresh = thresholded_image(img_corrected)
 img_corrected_proj = warp_perspective(img_corrected)
 img_thresh_proj = warp_perspective(img_thresh)
 
+left_fit, right_fit, left_curverad, right_curverad \
+    = find_lines_via_histogram_peaks(img_thresh_proj)
+ploty, left_fitx, right_fitx = get_plotting_values_for_fits(img_thresh_proj, left_fit, right_fit)
+# Create an image to draw the lines on
+warp_zero = np.zeros_like(img_thresh_proj).astype(np.uint8)
+img_thresh_proj_color = 255 * np.dstack((img_thresh_proj, img_thresh_proj, img_thresh_proj))
+color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+# Recast the x and y points into usable format for cv2.fillPoly()
+pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+pts = np.hstack((pts_left, pts_right))
+cv2.polylines(color_warp, np.int_([pts_left]), False, (255, 0, 0), 12)
+cv2.polylines(color_warp, np.int_([pts_right]), False, (255, 0, 0), 12)
+img_thresh_proj_color[color_warp[:,:,0] > 0] = 0
+polyplot = cv2.addWeighted(img_thresh_proj_color, 1, color_warp, 1, 1)
+fig = plt.figure()
+plt.title('Polynomial fit overlaid onto warped thresholded image')
+plt.imshow(polyplot)
+plt.show()
+
+'''
 fig = plt.figure()
 ax1 = fig.add_subplot(121)
 ax1.imshow(img_rgb)
@@ -35,6 +58,7 @@ ax2.set_title('Projected thresholded corrected image')
 plt.show()
 '''
 
+'''
 fig = plt.figure()
 images = glob.glob('../test_images/test*.jpg')
 plt.title('Thresholding characteristics of test images')
@@ -49,3 +73,4 @@ for i in range(0, 6):
     ax.set_title('test' + str(i+1) + '.jpg')
     ax.axis('off')
 plt.show()
+'''
